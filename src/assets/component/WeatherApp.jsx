90c98520cb3df88f0219a5bee55bce08391797a9
Kalpesh Talekar <kalpesh.talekar830@gmail.com>
2025-05-05 19:10:13 +0530

    weather app

--- src/assets/component/WeatherApp.jsx
+++ src/assets/component/WeatherApp.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
 
 const WeatherApp = () => {
-  return <div>WeatherApp</div>;
+  const [city, setCity] = useState();
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
+  return (
+    <>
+      <div className="appContainer">
+        <div className="appInner">
+          <input
+            type="text"
+            placeholder="Enter City Name"
+            value={city}
+            onChange={handleCityChange}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WeatherApp;
