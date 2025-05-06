40801e82e38ee46113e32d6a4aa3901116172a45
Kalpesh Talekar <kalpesh.talekar830@gmail.com>
2025-05-06 18:25:00 +0530

    New Weather app Created

--- src/assets/component/WeatherApp.jsx
+++ src/assets/component/WeatherApp.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
 import "./weather.css";
+import axios from "axios";
 
 const WeatherApp = () => {
   const [city, setCity] = useState();
+  const [weather, setweather] = useState();
+
   const handleCityChange = (event) => {
     setCity(event.target.value);
+  };
+  const fetchWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${"c87befeb7ef46c45c080a8296ab9b371"}`
+      );
+      setweather(response);
+    } catch (error) {
+      console.log("Error fetching weather data", error);
+    }
+  };
+
+  const handleGetWeather = () => {
+    fetchWeather();
   };
   return (
     <>
@@ -16,6 +33,16 @@ const WeatherApp = () => {
             value={city}
             onChange={handleCityChange}
           />
+          <button className="getWeatherBtn" onClick={handleGetWeather}>
+            Get Weather
+          </button>
+          {weather && (
+            <>
+              <div className="weather-info">
+                <h2>{weather.data.name}</h2>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
